db081ef71d52a9257a072cfbb1b12e115afa0c88
Abiywondimu5758 <abiywondimu1@gmail.com>
2023-11-28 18:44:54 +0300

    tests done

--- src/utils.py
+++ src/utils.py
@@ -44,60 +44,60 @@ def get_msgs_df_info(df):
     return msgs_count_dict, replies_count_dict, mentions_count_dict, links_count_dict
 
 
-def get_messages_dict(msgs):
-    msg_list = []
+# def get_messages_dict(msgs):
+#     msg_list = []
+#     for msg in msgs:
+#         for m in msg:
+#             message_type = m.get("subtype", "message")
+#             message_content = m.get("text", None)
+#             sender_name = m.get("user", None)
+#             time_sent = m.get("ts", None)
+#             message_distribution = "channel_join" if message_type == "channel_join" else "message"
 
-    for msg in msgs:
-        message_type = msg.get("subtype", "message")
-        message_content = msg.get("text", None)
-        sender_name = msg.get("user", None)
-        time_sent = msg.get("ts", None)
-        message_distribution = "channel_join" if message_type == "channel_join" else "message"
+#             msg_dict = {
+#                 "message_type": message_type,
+#                 "message_content": message_content,
+#                 "sender_name": sender_name,
+#                 "time_sent": time_sent,
+#                 "message_distribution": message_distribution,
+#                 "time_thread_start": m["ts"] if "parent_user_id" in m else None,
+#                 "reply_count": len(m["replies"]) if "thread_ts" in m and "reply_users" in m else None,
+#                 "reply_user_count": len(m["reply_users"]) if "thread_ts" in m and "reply_users" in m else None,
+#                 "time_thread_end": m["ts"] if "thread_ts" in m and "reply_users" in m else None,
+#                 "reply_users": m["reply_users"] if "thread_ts" in m and "reply_users" in m else None,
+#                 "blocks": [],
+#                 "emojis": [],
+#                 "mentions": [],
+#                 "links": [],
+#                 "link_count": 0
+#             }
 
-        msg_dict = {
-            "message_type": message_type,
-            "message_content": message_content,
-            "sender_name": sender_name,
-            "time_sent": time_sent,
-            "message_distribution": message_distribution,
-            "time_thread_start": msg["ts"] if "parent_user_id" in msg else None,
-            "reply_count": len(msg["replies"]) if "thread_ts" in msg and "reply_users" in msg else None,
-            "reply_user_count": len(msg["reply_users"]) if "thread_ts" in msg and "reply_users" in msg else None,
-            "time_thread_end": msg["ts"] if "thread_ts" in msg and "reply_users" in msg else None,
-            "reply_users": msg["reply_users"] if "thread_ts" in msg and "reply_users" in msg else None,
-            "blocks": [],
-            "emojis": [],
-            "mentions": [],
-            "links": [],
-            "link_count": 0
-        }
+#             if "blocks" in m and m["blocks"] is not None:
+#                 emoji_list = []
+#                 mention_list = []
+#                 links = []
 
-        if "blocks" in msg and msg["blocks"] is not None:
-            emoji_list = []
-            mention_list = []
-            links = []
+#                 for blk in m["blocks"]:
+#                     if "elements" in blk:
+#                         for elm in blk["elements"]:
+#                             if "elements" in elm:
+#                                 for elm_ in elm["elements"]:
+#                                     if "type" in elm_:
+#                                         if elm_["type"] == "emoji":
+#                                             emoji_list.append(elm_["name"])
+#                                         elif elm_["type"] == "user":
+#                                             mention_list.append(elm_["user_id"])
+#                                         elif elm_["type"] == "link":
+#                                             links.append(elm_["url"])
 
-            for blk in msg["blocks"]:
-                if "elements" in blk:
-                    for elm in blk["elements"]:
-                        if "elements" in elm:
-                            for elm_ in elm["elements"]:
-                                if "type" in elm_:
-                                    if elm_["type"] == "emoji":
-                                        emoji_list.append(elm_["name"])
-                                    elif elm_["type"] == "user":
-                                        mention_list.append(elm_["user_id"])
-                                    elif elm_["type"] == "link":
-                                        links.append(elm_["url"])
+#                 msg_dict["emojis"] = emoji_list
+#                 msg_dict["mentions"] = mention_list
+#                 msg_dict["links"] = links
+#                 msg_dict["link_count"] = len(links)
 
-            msg_dict["emojis"] = emoji_list
-            msg_dict["mentions"] = mention_list
-            msg_dict["links"] = links
-            msg_dict["link_count"] = len(links)
+#             msg_list.append(msg_dict)
 
-        msg_list.append(msg_dict)
-
-    return msg_list
+#     return msg_list
 
 
 def from_msg_get_replies(msg):
@@ -113,10 +113,79 @@ def from_msg_get_replies(msg):
     return replies
 
 
-def msgs_to_df(msgs):
-    msg_list = get_messages_dict(msgs)
+def get_messages_df(msgs):
+    msg_list = []
+
+    for msg in msgs:
+        for m in msg:
+            message_type = m.get("subtype", "message")
+            message_content = m.get("text", None)
+            sender_name = m.get("user", None)
+            time_sent = m.get("ts", None)
+            message_distribution = "channel_join" if message_type == "channel_join" else "message"
+
+            msg_dict = {
+                "message_type": message_type,
+                "message_content": message_content,
+                "sender_name": sender_name,
+                "time_sent": time_sent,
+                "message_distribution": message_distribution,
+                "time_thread_start": m["ts"] if "parent_user_id" in m else None,
+                "reply_count": len(m["replies"]) if "thread_ts" in m and "reply_users" in m else 0,
+                "reply_user_count": len(m["reply_users"]) if "thread_ts" in m and "reply_users" in m else 0,
+                "time_thread_end": m["ts"] if "thread_ts" in m and "reply_users" in m else None,
+                "reply_users": m["reply_users"] if "thread_ts" in m and "reply_users" in m else None,
+                "blocks": [],
+                "emojis": [],
+                "mentions": [],
+                "links": [],
+                "link_count": 0
+            }
+
+            if "blocks" in m and m["blocks"] is not None:
+                emoji_list = []
+                mention_list = []
+                links = []
+
+                for blk in m["blocks"]:
+                    if "elements" in blk:
+                        for elm in blk["elements"]:
+                            if "elements" in elm:
+                                for elm_ in elm["elements"]:
+                                    if "type" in elm_:
+                                        if elm_["type"] == "emoji":
+                                            emoji_list.append(elm_["name"])
+                                        elif elm_["type"] == "user":
+                                            mention_list.append(elm_["user_id"])
+                                        elif elm_["type"] == "link":
+                                            links.append(elm_["url"])
+
+                msg_dict["emojis"] = emoji_list
+                msg_dict["mentions"] = mention_list
+                msg_dict["links"] = links
+                msg_dict["link_count"] = len(links)
+
+            msg_list.append(msg_dict)
+
     df = pd.DataFrame(msg_list)
     return df
+
+
+def get_users_df(users):
+    df = pd.json_normalize(users)
+    columns = ['id', 'team_id', 'name', 'deleted', 'color', 'real_name', 'tz',
+               'tz_label', 'tz_offset', 'is_admin', 'is_owner', 'is_primary_owner',
+               'is_restricted', 'is_ultra_restricted', 'is_bot', 'is_app_user', 'profile.title', 'profile.phone', 'profile.skype', 'profile.real_name',
+               'profile.display_name', 'profile.status_text',
+               'profile.image_original', 'profile.email', 'profile.first_name', 'profile.last_name']
+    return df[columns]
+
+
+def get_channels_df(channels):
+    df = pd.json_normalize(channels)
+    columns = ['id', 'name', 'created', 'creator', 'is_general',
+               'members']
+    return df[columns]
 
 
 def process_msgs(msg):
@@ -139,7 +208,7 @@ def get_messages_from_channel(channel_path):
     channel_json_files = os.listdir(channel_path)
     channel_msgs = [json.load(open(channel_path + "/" + f)) for f in channel_json_files]
 
-    df = pd.concat([pd.DataFrame(get_messages_dict(msgs)) for msgs in channel_msgs])
+    df = pd.concat([get_messages_df(msgs) for msgs in channel_msgs])
     print(f"Number of messages in channel: {len(df)}")
 
     return df
---
+++ tests/test_slack_data_loader.py
@@ -0,0 +1,54 @@
+
+import unittest
+import pandas as pd
+from src import utils
+from src.loader import SlackDataLoader
+
+
+class TestSlackDataLoader(unittest.TestCase):
+    def setUp(self):
+        self.data_path = 'C:/Users/Abiy/Documents/Projects/10 Academy/10academy_week_0_/anonymized'
+        self.loader = SlackDataLoader(self.data_path)
+
+    def test_get_users_expected_columns(self):
+        users = self.loader.get_users()
+        users_df = utils.get_users_df(users)
+        expected_columns = ['id', 'team_id', 'name', 'deleted', 'color', 'real_name', 'tz',
+                            'tz_label', 'tz_offset', 'is_admin', 'is_owner', 'is_primary_owner',
+                            'is_restricted', 'is_ultra_restricted', 'is_bot', 'is_app_user', 'profile.title', 'profile.phone', 'profile.skype', 'profile.real_name',
+                            'profile.display_name', 'profile.status_text',
+                            'profile.image_original', 'profile.email', 'profile.first_name', 'profile.last_name']
+        self.assertListEqual(list(users_df.columns), expected_columns)
+
+    def test_get_channels_expected_columns(self):
+        channels = self.loader.get_channels()
+        channels_df = utils.get_channels_df(channels)
+        expected_columns = ['id', 'name', 'created', 'creator', 'is_general',
+                            'members']
+        self.assertListEqual(list(channels_df.columns), expected_columns)
+
+    def test_get_channel_messages_expected_columns(self):
+        channel_name = 'all-broadcast'
+        channel_messages = self.loader.get_channel_messages(channel_name)
+        channel_messages_df = utils.get_messages_df(channel_messages)
+        # print(channel_messages_df.head())
+        expected_columns = ["message_type",
+                            "message_content",
+                            "sender_name",
+                            "time_sent",
+                            "message_distribution",
+                            "time_thread_start",
+                            "reply_count",
+                            "reply_user_count",
+                            "time_thread_end",
+                            "reply_users",
+                            "blocks",
+                            "emojis",
+                            "mentions",
+                            "links",
+                            "link_count",]
+        self.assertListEqual(list(channel_messages_df.columns), expected_columns)
+
+
+if __name__ == '__main__':
+    unittest.main()
